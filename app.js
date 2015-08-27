ec77af8ddffaa063d2f9e5c8ef289e13faf48eb1
Manu Lohiya <manulohiya@gmail.com>
2015-08-27 15:53:47 -0700

    routing

---
+++ app.js
@@ -0,0 +1,22 @@
+angular.module('gitAroundApp', ['ngRoute', 'gitAroundApp.controllers'])
+
+.config(['$routeProvider',
+   function ($routeProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: 'templates/home.html',
+                controller: 'ItinsCtrl'
+            })
+         
+            .otherwise({
+                redirectTo: '/'
+            });
+}])
+
+
+
+
+
+
+
+;
---
+++ controllers.js
@@ -0,0 +1,11 @@
+angular.module('gitAroundApp.controllers', [])
+
+.controller('ItinsCtrl', ['$scope', function($scope) {
+
+	console.log("Controller is working like a BOSS")
+    
+}])
+
+
+
+
