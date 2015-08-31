f07c010192751d4c2388e61e4d175fcd976ef8a9
Manu Lohiya <manulohiya@gmail.com>
2015-08-30 22:33:19 -0700

    show items on a map

--- controllers.js
+++ controllers.js
@@ -34,24 +34,99 @@ angular.module('gitAroundApp.controllers', ['gitAroundApp.services', 'uiGmapgoog
 
 
 .controller('ItinshowCtrl', ['$scope', '$routeParams', 'ItinService', function($scope, $routeParams, ItinService) {
-   var itin_id = $routeParams.id;
-   console.log("Id: "+itin_id)
-	console.log("Show Controller is working like a BOSS")
-   
- //    ItinService.get({id:itin_id}, function(response){
- //   $scope.items = response.items
- //    console.log($scope.items)
- // });
-
+      
      
-   $scope.itin = ItinService.get(itin_id)
-    console.log($scope.itin)
- 
+
+      var itin_id = $routeParams.id;
+      console.log("Id: "+itin_id)
+      console.log("Show Controller is working like a BOSS")
+
+      $scope.itin = ItinService.get(itin_id)
+      console.log($scope.itin)
+      $scope.items = $scope.itin.items
+      console.log($scope.items)
+      console.log("Number of items:"+$scope.items.length)
+      $scope.render = false
+      
+      
+      $scope.map = { center: 
+         { latitude: 37.7833, longitude: -122.4167 
+         }, 
+         zoom: 12
+      }
+
+      var codeAddress = function(address, onSuccess) {
+         console.log('address is:'+ address);
+         geocoder = new google.maps.Geocoder();
+         console.log("Requesting geocode...");
+         geocoder.geocode({'address': address}, function(results, status) {
+            console.log("Received geocode with status" + status);
+            if (status == google.maps.GeocoderStatus.OK) {
+               var latLng = results[0].geometry.location;
+               console.log(latLng)
+               onSuccess(latLng);
+               //return lat;
+               // console.log("map works!")
+               //  console.log(results)
+               //  console.log(results[0].geometry.location.G)
+               //  console.log(results[0].geometry.location.K)
+            }
+            else {
+               console.log("phuck uuuuu " + status);
+            };
+         });
+      };
+
+      // var address = 'Stow Lake Boathouse, Stow Lake Drive, San Francisco, CA, United States';
+      var i = 0;
+      // while (i < 5) {
+      codeAddress($scope.items[i].location,function(latLng){
+         console.log("Lat recieved with " + latLng);
+            
+         // $scope.marker = {
+         //    id: $scope.items[i].id,
+         //    coords: 
+         //    {   
+         //       latitude: latLng.G,
+         //       longitude: latLng.K
+         //    }
+         
+         // };
+          
+      
+      $scope.markerList = [{
+
+         id: $scope.items[i].id,
+         coords: {   
+            latitude: latLng.G,
+            longitude: latLng.K
+         }
+
+      },
+      {id: $scope.items[1].id,
+       coords: {   
+         latitude: 37.7900,longitude: -122.4167
+      }
+
+      }]
+
+
+
+      // console.log($scope.marker)
+      // console.log($scope.markerList)
+
+      $scope.render=true
+      }); 
+
+   
+   }
+
 
     
-}])
+      
+  
 
-;
+]);
 
 
 
--- services.js
+++ services.js
@@ -12,35 +12,40 @@ angular.module('gitAroundApp.services', ['uiGmapgoogle-maps'])
     itin_duration_days: 1,
     city: 'San Francisco',
     items: [
-      {location: 'Stow Lake Boathouse, Stow Lake Drive, San Francisco, CA, United States',
+      {id: 0,
+        location: 'Stow Lake Boathouse, Stow Lake Drive, San Francisco, CA, United States',
       name: 'Boating in Stow Lake ',
       desc: 'After having breakfast at tartine, lets head to Golden Gate Park and do some exercise! Stow Lake is one of the biggest lake in the park. Rowboats and paddle boat can be rented at boathouse. If you feel like exploring golden gate park instead, you may also rent a bike at the opposite of rental spot. [INFO] 50 Stow Lake Dr | San Francisco, CA 94118 | (415) 752-0347 | Hours: Mo to Su from 10:00am to 04:00pm. [TIP] Parking is easy, you may either park at parking lot or around the lake. ',
       category: 'Play',
       image: 'http://stowlakeboathouse.com/wp-content/uploads/2014/05/stowe_lake_02.jpg'
 
       },
-      {location: 'Tartine bakery, Guerrero Street, San Francisco, CA, United States',
+      {id: 1,
+        location: 'Tartine bakery, Guerrero Street, San Francisco, CA, United States',
       name: 'Start the day with baked goods!',
       desc: 'Lets start a day with bread pudding and fresh grape fruit juice at Tartine Bakery. Its one of the best bakery-cafe in town. They make amazing breads, pastries and open faced melted cheese sandwiches(croques monsieurs). My favorite item is their famous bread pudding, baked with fresh season fresh fruit, must try! They always have a long line on weekends and special holidays. Go early to avoid the line! Tables are limited. If you dont feel like struggling with tables, bring a picnic mat, Mission Dolores Park is just one block away(turn left on 18th st when walking out of the Tartine). [INFO] 600 Guerrero Street | San Francisco, CA 94110-1528 | (415) 487-2600. [TIP] Take bus and get off at 18th & Guerrero St. Parking is limited in the neighbor hood and require excellent parallel parking skill. There are few private parking lot located on 18th heading toward Valencia st. ',
       category: 'Eat',
       image: 'http://www.zomppa.com/wp-content/uploads/2011/04/DSC_0022.jpg'
 
       },
-      {location: 'Irving St & 9th Ave',
+      {id: 2,
+        location: 'Irving St & 9th Ave',
       name: ' Lunch in inner sunset ',
       desc: 'What for lunch? I would suggest go Irving street at the south of park. Irving street and 9th avenue is densely packed with diverse eateries. Few good choice will be Naan n Curry on Irving St. & 8th(Indian), Pluto on Irving & 7th(Sandwich & Salad), Park Chow in 9th Ave between Irving st and Lincoln way. Pacific catch on 9th ave between Irving and Lincoln way. My most recommend place will be San Tung Chinese Restaurant on Irving t between 11th ave and 12 ave. Their shrimp and leek dumplings and dry fried chicken wings are the best I ever had in bay area. [TIP] Parking can be difficult, and you may need to drive a couple of blocks up into the Avenues to find a space. ',
       category: 'Eat',
       image: 'http://1.bp.blogspot.com/_pN2ZiUZWULg/TJcAfcpWQkI/AAAAAAAAAPQ/DEcm1eU9ZHI/s1600/N+Judah+at+9th+%26+Irving.jpg'
 
       },
-      {location: 'California Academy of Sciences, Music Concourse Drive, San Francisco, CA, United States',
+      {id: 3,
+        location: 'California Academy of Sciences, Music Concourse Drive, San Francisco, CA, United States',
       name: 'Spend the afternoon at the museum',
       desc: 'After lunch, we may head back to the park and go to the world famous Academy of Sciences. Its a place that you can simply spend all day in it. The California Academy of Science is a multifaceted scientific institution committed to leading-edge research, to educational outreach, and to finding new and innovative ways to engage and inspire the public.- quote from their site. In my word, it would be: They have indoor Rain forest, aquarium, and 3D movie planetarium. Its way too cool. [Tip] 1. Make sure u get the movie ticket right when you walk into the museum, Its always an hour or 2 hours wait for the movie time. Also, check Rain forest first if your time is limited. It always has a line and close earlier than aquarium. There never be a line to aquarium. 2.On summer Thursday nights, they offer half price happy hour tickets and serve wine and beer. Fore more detail please check their website. ',
       category: 'Play',
       image: 'http://www.calacademy.org/sites/default/files/styles/manual_crop_standard_960x540/public/assets/images/KW_IMAGES_DO_NOT_USE/admission-.jpg?itok=Msauwoc1'
 
       },
-      {location: 'Haight-Ashbury, San Francisco, CA, United States',
+      {id: 4,
+        location: 'Haight-Ashbury, San Francisco, CA, United States',
       name: 'Head to Haight Ashbury',
       desc: 'After a long day in golden gate park area, Lets head to Haight-Ashbury street and see the other face of San Francisco. If you are curious about the San Francisco hippies culture at 60s. This is where to find it. Walk down the street you will see all those fun clothing, accessory and toy stores that pursuit the spirits of hippies. At the end of the street, you will see the restaurant Cha Cha Cha at your left(1801 Haight st), at the corner of shredder, this is our end up dinner place! Woo!(Yes, I will never forget about food). They offer small tapas with reasonable price and good sangria.',
       category: 'Play',
